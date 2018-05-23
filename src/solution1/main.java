228520306a659f81019c1261b749e41572cb29be
JeremyCoxBMI <jeremy.cox.cs@gmail.com>
2018-05-23 16:14:31 -0400

    A solution

--- src/solution1/main.java
+++ src/solution1/main.java
@@ -27,19 +27,30 @@ import java.util.ArrayList;
  * in O(log n) time, but not end times.
  *
  * Here we realize a truth that gives us an edge:
- * we can test
- *      b.start is between a.start and b.end
- *          separately from
- *      a.start is between b.start and a.end
+
+ * if we parse over the array using adjacent pairs, we know we have the
+ * second entr (b) y is the closest start time after the first entry (a)
  *
- *  This gives the advantage that when testing for conflicts with b, we do not need to search for end times in a
+ * Thus
+ *      a.start is between b.start and b.end is always FALSE
  *
- *  This relationship works because conflicted appointments are commutative.
- *  In otherwords, if we test a vs b and then b vs a,
+ *      so we need only test
  *
+ *      b.start is between a.start and a.end
  *
- *  So, we can run through entries and test for conflicts by closest start time only,
- *  which is the next entry in the sorted list.
+ *  What if we loop and just check the next entry as a conflict?
+ *      Is it possible that out of three entries, a, b, and c that c conflicts with a, but not b?
+ *
+ *  if b does not conflict with c,
+ *      then c.start >= b.end
+ *  if c conflicts with a,
+ *      then a.start <= c.start and c.start < a.end
+ *
+ *  if b.end < a.end, this case is entirely possible
+ *  therefore, we cannot strictly just do the next comparison
+ *
+ * Strictyly speaking, this runs in O(n^2) time,
+ * but because P(conflict between and b) is very small, we expect much better average time
  *
  *
  */
@@ -54,9 +65,10 @@ public class main {
         ArrayList<CalendarEntry> arr = new ArrayList<>();
 
         for (int k=0; k < arr.size()-1; k++){
-            if ( isStartIn(arr.get(k+1).getStart(),arr.get(k))){
-                arr.get(k).setConflict(true);
-                arr.get(k+1).setConflict(true);
+            //we already know that arr.get(k).getStart() <= arr.get(j).getStart()
+            for (int j = k + 1; arr.get(j).getStart() < arr.get(k).getEnd() && j < arr.size() ; j++) {
+                    arr.get(k).setConflict(true);
+                    arr.get(j).setConflict(true);
             }
         }
     }
